c01e1540bdbbe1a691cee60b1172abd94d9ea8a2
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-25 22:53:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@
 class Artist
   attr_accessor :name
   attr_reader :songs
+  
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
@@ -26,8 +27,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+  
 end
