153df342e8a34a37202d6800c7fd563b920db4d2
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-25 22:48:07 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,6 @@
 module Findable
-
-  def self.find_by_name(name)
-    @@all.detect{|a| a.name == name}
-  end
+  module ClassMethods
+    def self.find_by_name(name)
+      @@all.detect{|a| a.name == name}
+    end
 end
-
-  
